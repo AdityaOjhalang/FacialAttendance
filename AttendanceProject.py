82f843e5b5d84a34078db20399ba55a6391cf310
Aditya Ojha <aditya.ojha2820@gmail.com>
2022-01-05 21:41:04 +0530

    Facial Attendace system

---
+++ AttendanceProject.py
@@ -0,0 +1,64 @@
+import face_recognition
+import cv2
+import numpy as np
+import os
+from datetime import datetime
+
+path = 'ImagesAttendance'
+images = []     # LIST CONTAINING ALL THE IMAGES
+className = []    # LIST CONTAINING ALL THE CORRESPONDING CLASS Names
+myList = os.listdir(path)
+print("Total Classes Detected:",len(myList))
+for x,cl in enumerate(myList):
+        curImg = cv2.imread(f'{path}/{cl}')
+        images.append(curImg)
+        className.append(os.path.splitext(cl)[0])
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+def markAttendance(name):
+    with open('Attendance.csv','r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dt_string = now.strftime("%H:%M:%S")
+            f.writelines(f'\n{name},{dt_string}')
+
+encodeListKnown = findEncodings(images)
+print('Encodings Complete')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    imgS = cv2.resize(img, (0, 0), fx=0.25, fy=0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+            matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+            print(faceDis)
+
+            matchIndex = np.argmin(faceDis)
+            if faceDis[matchIndex] < 0.50:
+                    name = className [matchIndex].upper()
+                    markAttendance(name)
+
+            else:
+                name = 'Unknown'
+            print(name)
+
+
